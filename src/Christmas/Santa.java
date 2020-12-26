d85de2b1e5ac325b31befff0c5ce74ef9475299d
Evan Mead <evan.c.mead@gmail.com>
2020-12-26 14:25:17 -0600

    reindeer class created - reindeer created in santa class

--- src/Christmas/Santa.java
+++ src/Christmas/Santa.java
@@ -6,6 +6,7 @@ public class Santa {
 
     public static void main(String[] args) {
         askSanta();
+        createReindeer();
     }
 
     public static void askSanta() {
@@ -36,7 +37,15 @@ public class Santa {
 
         System.out.println("What would you like for Christmas?\n");
         String userString = sc.nextLine();
-        System.out.printf("\nWell...if you are good, I think I can manage to bring you %s for your Christmas gift!", userString);
+        System.out.printf("\nWell...if you are good, I think I can manage to bring you %s for your Christmas gift!\n", userString);
+    }
+
+    public static void createReindeer() {
+        Reindeer reindeer1 = new Reindeer("Dasher");
+        Reindeer reindeer2 = new Reindeer("Prancer");
+
+        System.out.printf("\nSanta's first reinder is %s.", reindeer1.name);
+        System.out.printf("\nSanta's second reindeer is %s.", reindeer2.name);
     }
 
 }
