c6a2594c2f14a559fa7fc7db8345c76827020916
Evan Mead <evan.c.mead@gmail.com>
2020-12-25 22:13:24 -0600

    christmas tree text art added

--- src/Christmas/Santa.java
+++ src/Christmas/Santa.java
@@ -11,6 +11,29 @@ public class Santa {
     public static void askSanta() {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("           *             ,\n" +
+                "                       _/^\\_\n" +
+                "                      <     >\n" +
+                "     *                 /.-.\\         *\n" +
+                "              *        `/&\\`                   *\n" +
+                "                      ,@.*;@,\n" +
+                "                     /_o.I %_\\    *\n" +
+                "        *           (`'--:o(_@;\n" +
+                "                   /`;--.,__ `')             *\n" +
+                "                  ;@`o % O,*`'`&\\\n" +
+                "            *    (`'--)_@ ;o %'()\\      *\n" +
+                "                 /`;--._`''--._O'@;\n" +
+                "                /&*,()~o`;-.,_ `\"\"`)\n" +
+                "     *          /`,@ ;+& () o*`;-';\\\n" +
+                "               (`\"\"--.,_0 +% @' &()\\\n" +
+                "               /-.,_    ``''--....-'`)  *\n" +
+                "          *    /@%;o`:;'--,.__   __.'\\\n" +
+                "              ;*,&(); @ % &^;~`\"`o;@();         *\n" +
+                "              /(); o^~; & ().o@*&`;&%O\\\n" +
+                "              `\"=\"==\"\"==,,,.,=\"==\"===\"`\n" +
+                "           __.----.--.''#####---...___...-----._\n" +
+                "         '`            `\"\"\"\"\"`\n");
+
         System.out.println("What would you like for Christmas?\n");
         String userString = sc.nextLine();
         System.out.printf("\nWell...if you are good, I think I can manage to bring you %s for your Christmas gift!", userString);
